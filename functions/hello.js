f8c847efe0b66043be71a656122d314694a60a5c
zeeberry <zainabebrahimi@gmail.com>
2020-07-02 14:52:29 -0400

    adding and trying graphql

--- functions/hello.js
+++ functions/hello.js
@@ -1,5 +1,16 @@
+const { graphql, buildSchema } = require('graphql');
+
 exports.handler = async event => {
   const subject = event.queryStringParameters.name || 'World'
+
+  const schema = buildSchema(`
+    type Query {
+      hello: String
+    }
+` );
+
+  console.log('SCHEMA:', schema);
+
   return {
     statusCode: 200,
     body: `Oh, Hello ${subject}!`,
